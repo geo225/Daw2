0aa8ad8d144c4841ccdc633c1618229b0082cd34
warrock225@gmail.com <warrock225@gmail.com>
2018-02-19 16:36:41 +0100

    factura

--- Proyecto DWEC/factura.php
+++ Proyecto DWEC/factura.php
@@ -19,32 +19,161 @@ for ($i=0;$i<count($_SESSION['carrito']->getProductos());$i++){
         }
 ob_start(); //Start output buffer
 for ($i=0;$i<count($_SESSION['carrito']->getProductos());$i++){
-    echo "<p> Marca:  $Marca[$i]</p>";
-    echo "<p> Nombre: $Nombre[$i]</p>";
-    echo "<p> Precio: $Precio[$i]€</p>";
-    echo "<p> Cantidad: ".$_GET['cantidad'.$i]."</p>";    
+    echo "<tr>";
+    echo "<td>$Marca[$i]</td>";
+    echo "<td>$Nombre[$i]</td>";
+    echo "<td>$Precio[$i]€</td>";
+    echo "<td>".$_GET['cantidad'.$i]."</td>"; 
+    echo "</tr>";
 };
-    echo '<p style="color:red"> Total:'.$_GET['total'].'</p>';
+    echo "<tr>";
+    echo '<td colspan="2" style="border: 1px transparent solid;background-color: white;"></td>';
+    echo "<th>Total</th>";
+    echo '<td style="color:red;border: 1px solid black;">'.$_GET['total'].'</td>';
+    echo "<tr>";
 $output = ob_get_contents(); //Grab output
 ob_end_clean();
-$html='
-<html>
+$html='<html>
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Ejemplo de Documento en PDF.</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Ejemplo de Documento en PDF.</title>
+    <style>
+        html {
+            width: 210mm;
+            height: 297mm;
+        }
+
+        body {
+            width: 97%;
+            margin: auto;
+            height: 100%;
+        }
+
+        .cabezera {
+            width: 97%;
+            background-color: black;
+            text-align: center;
+            color: white;
+            margin: auto;
+        }
+
+        .logoEmpresa {
+            width: 65%;
+            text-align: right;
+            display: inline-block;
+        }
+
+        .Empresa {
+            width: 100%;
+            margin: auto;
+            display: inline-block;
+            text-align: right;
+        }
+        .main{
+            width: 100%;
+        }
+        table,
+        th,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        th,
+        td {
+            padding: 5px;
+            text-align: left;
+        }
+
+        th {
+            background-color: lightgray;
+        }
+
+        .datos {
+            width: 99%;
+        }
+
+        .productos {
+            width: 100%;
+            clear: both;
+        }
+
+        table#t01 tr:nth-child(even) {
+            background-color: #eee;
+        }
+
+        table#t01 tr:nth-child(odd) {
+            background-color: #fff;
+        }
+
+        table#t01 th {
+            background-color: black;
+            color: white;
+        }
+
+    </style>
 </head>
-<body style="width:100%;margin:auto;text-align:center">
 
-<h2>Datos Personales:</h2>
-<p>Nombre: '.$nombre.'</p>
-<p>Apellidos: '.$apellidos.'</p>
-<p>Direccion: '.$direccion.'</p>
-<p>Correo Electronico: '.$email.'
-<h2>Productos:</h2>    
-'.$output.'    
+<body>
+
+    <div class="cabezera">
+        <h1> Factura de Compra</h1>
+    </div>
+    <div class="main">
+        <div class="Empresa">
+            <p><strong> Tienda Rodrigo</strong></p>
+            <p><strong> Calle Falsa 123</strong></p>
+            <p><strong> Zamora, Benavente</strong></p>
+            <p><strong> 980630000</strong></p>
+        </div>
+        <div class="LogoEmpresa">
+            <img src="webroot/img/Razer-logo.png" height="152px" width="152px">
+        </div>
+        <div class="datos">
+            <table style="width: 60%;">
+                <caption>
+                    <h2>Datos Facturacion</h2>
+                </caption>
+                <tr>
+                    <th>Nombre</th>
+                    <td>'.$nombre.'</td>
+                </tr>
+                <tr>
+                    <th>Apellidos</th>
+                    <td>'.$apellidos.'</td>
+                </tr>
+                <tr>
+                    <th>Direccion</th>
+                    <td>'.$direccion.'</td>
+                </tr>
+                <tr>
+                    <th>Correo Electronico</th>
+                    <td>'.$email.'</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <div class="productos" style="margin-top:30px;">
+        <table id="t01" style="width: 100%;">
+            <caption>
+                <h2>Productos</h2>
+            </caption>
+            <tr>
+                <th>Marca</th>
+                <th>Nombre</th>
+                <th>Cantidad</th>
+                <th>Precio</th>
+            </tr>
+            '.$output.'
+        </table>
+    </div>
 </body>
-</html>';
 
+</html>
+
+';
+//echo $html;
 // reference the Dompdf namespace
 use Dompdf\Dompdf;
 
